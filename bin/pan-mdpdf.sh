141483517eb6bc3f8f561f6dec9a8a2910a6ca80
btmonier <brandon.monier@gmail.com>
2018-01-12 12:59:38 -0600

    Fix typos and bugs

--- bin/pan-mdpdf.sh
+++ bin/pan-mdpdf.sh
@@ -4,11 +4,11 @@
 # Title:         Pandoc - Markdown to PDF using Template
 # Author:        Brandon Monier
 # Created:       2018-01-12 at 12:38:03
-# Last Modified: 2018-01-12 at 12:42:10
+# Last Modified: 2018-01-12 at 12:58:41
 #---------------------------------------------------------------------
 
 # Variables
-$1
+VAR=$1
 
 # Script
-pandoc --TOC -s $1md - $1pdf --from markdown --template eisvogel --listings
+pandoc --toc -s $VAR.md -o $VAR.pdf --from markdown --template eisvogel --listings
