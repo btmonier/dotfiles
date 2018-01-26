e8b4c5195139db5d87010eb6ea1c41941bc725e8
btmonier <brandon.monier@gmail.com>
2018-01-26 12:40:27 -0600

    Open with mupdf

--- bin/pan-mdpdf.sh
+++ bin/pan-mdpdf.sh
@@ -4,11 +4,11 @@
 # Title:         Pandoc - Markdown to PDF using Template
 # Author:        Brandon Monier
 # Created:       2018-01-12 at 12:38:03
-# Last Modified: 2018-01-12 at 12:58:41
+# Last Modified: 2018-01-26 at 12:21:04
 #---------------------------------------------------------------------
 
 # Variables
 VAR=$1
 
 # Script
-pandoc --toc -s $VAR.md -o $VAR.pdf --from markdown --template eisvogel --listings
+pandoc --toc -s $VAR.md -o $VAR.pdf --from markdown --template eisvogel --listings && mupdf $VAR.pdf
