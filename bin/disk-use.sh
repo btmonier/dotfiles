72334f9ef8396d977d18bc873ffb68ddd374bee3
btmonier <brandon.monier@gmail.com>
2018-02-24 16:48:56 -0600

    Initial commit

---
+++ bin/disk-use.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------
+# Title:         Disk Space Visualization with R
+# Author:        Brandon Monier
+# Created:       2018-02-24 at 16:06:18
+# Last Modified: 2018-02-24 at 16:35:23
+#---------------------------------------------------------------------
+
+df > ~/tmp/tmp-storage
+
+R CMD BATCH ~/dev/R/misc-scripts/disk-usage-vis.R
+
+rm ~/tmp/tmp-storage
+
+echo "Viewing image..."
+
+DATE=`date +%Y-%m-%d`
+mupdf ~/media/pics/system/disk-usage-$DATE.pdf
